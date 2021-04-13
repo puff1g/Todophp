bf6229e77204a431b7a65d1403274eba6d1cecad
DESKTOP-2VFB9CV\tke20 <tke2000wow@gmail.com>
2021-04-13 22:40:35 +0200

    Changes yet errrororosrors

--- index.php
+++ index.php
@@ -1,91 +1,46 @@
+<?php 
+  session_start(); 
+
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: login.php");
+  }
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="./css/style.css">
-    <link rel="stylesheet" href="./css/login.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script type="text/javascript" src="./css/javascript/functions.js"></script>
-
-
-    <title>TODO</title>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<?php
-            require_once('components/dbcon.php');
-            $DBcon = new DBcon();
-            $connectionStatus = $DBcon->get_connection();
-        ?>
 
-        <?php if($connectionStatus == true) :  ?>
-            <div id="back">
-    <canvas id="canvas" class="canvas-back"></canvas>
-    <div class="backRight">    
-    </div>
-    <div class="backLeft">
-    </div>
+<div class="header">
+	<h2>Home Page</h2>
 </div>
+<div class="content">
+  	<!-- notification message -->
+  	<?php if (isset($_SESSION['success'])) : ?>
+      <div class="error success" >
+      	<h3>
+          <?php 
+          	echo $_SESSION['success']; 
+          	unset($_SESSION['success']);
+          ?>
+      	</h3>
+      </div>
+  	<?php endif ?>
 
-<div id="slideBox">
-    <div class="topLayer">
-    <div class="left">
-        <div class="content">
-        <h2>Sign Up</h2>
-        <form id="form-signup" method="post" onsubmit="return false;">
-            <div class="form-element form-stack">
-            <label for="email" class="form-label">Email</label>
-            <input id="email" type="email" name="email">
-        </div>
-        <div class="form-element form-stack">
-            <label for="username-signup" class="form-label">Username</label>
-            <input id="username-signup" type="text" name="username">
-        </div>
-        <div class="form-element form-stack">
-            <label for="password-signup" class="form-label">Password</label>
-            <input id="password-signup" type="password" name="password">
-        </div>
-        <div class="form-element form-checkbox">
-            <input id="confirm-terms" type="checkbox" name="confirm" value="yes" class="checkbox">
-            <label for="confirm-terms">I agree to the <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a></label>
-        </div>
-        <div class="form-element form-submit">
-            <button id="signUp" class="signup" type="submit" name="signup">Sign up</button>
-            <button id="goLeft" class="signup off">Log In</button> 
-        </div>
-        </form>
-        </div>
-    </div>
-    <div class="right">
-        <div class="content">
-        <h2>Login</h2>
-        <form id="form-login" method="post" onsubmit="return false;">
-            <div class="form-element form-stack">
-            <label for="username-login" class="form-label">Username</label>
-            <input id="username-login" type="text" name="username">
-            </div>
-            <div class="form-element form-stack">
-            <label for="password-login" class="form-label">Password</label>
-            <input id="password-login" type="password" name="password">
-            </div>
-            <div class="form-element form-submit">
-            <button id="logIn" class="login" type="submit" name="login">Log In</button>
-            <button id="goRight" class="login off" name="signup">Sign Up</button>
-            </div>
-        </form>
-        </div>
-    </div>
-    </div>
+    <!-- logged in user information -->
+    <?php  if (isset($_SESSION['username'])) : ?>
+    	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+    	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+    <?php endif ?>
 </div>
-
-        <?php else : ?>
-
-            <h1>nah fam...</h1>
-            <h2><?php echo $connectionStatus; ?></h2>
-            <?php endif; ?> 
-
+		
 </body>
 </html>
